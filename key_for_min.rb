f68209085f1dad2b97978fb8f65d9cf675c12035
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-29 19:02:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 0
   min_key = 0
-  name_hash.collect do |d_key, d_value|
+  name_hash.each do |d_key, d_value|
     if d_value < min_value
       min_key = d_key
       min_value = d_value
